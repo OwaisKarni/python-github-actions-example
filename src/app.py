fbeaccc2ac3bbfe9acf83437409892f42d7db0d1
Owais Karni <owaiskarni81@hotmail.com>
2021-09-15 17:25:36 +0500

    test for post request

--- src/app.py
+++ src/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello, world!"
 
 
+@app.route('/message', methods=['POST'])
+def route_message():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        message = request_data['message']
+        response = json.dumps({'message': message})
+        return response
+
+
 if __name__ == "__main__":
     app.run()
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
